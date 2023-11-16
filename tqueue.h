87ea1a11153c5dd17997335e367f91c33ebb8679
lietzdaniel <lietz_daniel@gmx.de>
2023-11-16 11:29:30 +0100

    Implemented Header File For threaded queue

--- tqueue.h
+++ tqueue.h
@@ -1,2 +1,28 @@
+#include <queue>
+#include <mutex>
+
+template<typename T>
+class tqueue {
+    std::queue<T> internal_queue;
+    std::mutex queue_write_mutex;
+    
+    void lock_mutex();
+    void unlock_mutex();
+    public:
+        queue();
+        T& front();
+        const T& front() const;
+        T& back();
+        const T& back() const;
+        bool empty() const;
+        size_t size() const;
+        void push( T&& value );
+        void push_range( R&& rg );
+        decltype(auto) emplace( Args&&... args );
+        void pop();
+        void swap( tqueue& other )
+        tqueue& operator=( const tqueue& other );
+    
 
 
+};
