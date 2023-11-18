8bb9613a320902281506d2142ef48e13e398e2c4
lietzdaniel <lietz_daniel@gmx.de>
2023-11-18 16:29:59 +0100

    restructured project and implemented tests for TQueue

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # stock-simulator
 Application simulating a stock data feed.
+
+
+## Running the Program
+
+1. `mkdir build`
+2. `cd build`
+3. `cmake ..`
+4. `make`
+
+Currently, there is no executable, only `tqueue`
---
+++ tests/tqueue_general_tests.cpp
@@ -0,0 +1,101 @@
+#include <gtest/gtest.h>
+#include "tqueue.h"
+
+TEST(TQueueTest, Constructor) {
+    EXPECT_NO_THROW(tqueue<std::string> t_queue;);
+    EXPECT_NO_THROW(tqueue<int> t_queue;);
+    EXPECT_NO_THROW(tqueue<char> t_queue;);
+}
+
+TEST(TQueueTest, Empty) {
+    tqueue<std::string> t_queue;
+    EXPECT_EQ(t_queue.empty(), true);
+    t_queue.push("Hello");
+    EXPECT_EQ(t_queue.empty(), false);
+    t_queue.push("World");
+    EXPECT_EQ(t_queue.empty(), false);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.empty(), false);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.empty(), true);
+}
+
+TEST(TQueueTest, Size) {
+    tqueue<std::string> t_queue;
+    EXPECT_EQ(t_queue.size(), 0);
+    t_queue.push("Hello");
+    EXPECT_EQ(t_queue.size(), 1);
+    t_queue.push("World");
+    EXPECT_EQ(t_queue.size(), 2);
+    t_queue.push("!");
+    EXPECT_EQ(t_queue.size(), 3);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.size(), 2);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.size(), 1);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.size(), 0);
+}
+
+
+TEST(TQueueTest, PushPop) {
+    tqueue<std::string> t_queue;
+    t_queue.push("Hello");
+    EXPECT_EQ(t_queue.front(), "Hello");
+    EXPECT_EQ(t_queue.back(), "Hello");
+    EXPECT_EQ(t_queue.size(), 1);
+    t_queue.push("World");
+    EXPECT_EQ(t_queue.front(), "Hello");
+    EXPECT_EQ(t_queue.back(), "World");
+    EXPECT_EQ(t_queue.size(), 2);
+    t_queue.push("!");
+    EXPECT_EQ(t_queue.front(), "Hello");
+    EXPECT_EQ(t_queue.back(), "!");
+    EXPECT_EQ(t_queue.size(), 3);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.front(), "World");
+    EXPECT_EQ(t_queue.back(), "!");
+    EXPECT_EQ(t_queue.size(), 2);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.front(), "!");
+    EXPECT_EQ(t_queue.back(), "!");
+    EXPECT_EQ(t_queue.size(), 1);
+    t_queue.pop();
+    EXPECT_EQ(t_queue.size(), 0);
+}
+
+
+TEST(TQueueTest, front) {
+    tqueue<std::string> t_queue;
+    t_queue.push("Hello");
+    EXPECT_EQ(t_queue.front(), "Hello");
+    t_queue.push("World");
+    EXPECT_EQ(t_queue.front(), "Hello");
+    t_queue.push("!");
+    EXPECT_EQ(t_queue.front(), "Hello");
+    t_queue.pop();
+    EXPECT_EQ(t_queue.front(), "World");
+    t_queue.pop();
+    EXPECT_EQ(t_queue.front(), "!");
+    t_queue.pop();
+}
+
+TEST(TQueueTest, back) {
+    tqueue<std::string> t_queue;
+    t_queue.push("Hello");
+    EXPECT_EQ(t_queue.back(), "Hello");
+    t_queue.push("World");
+    EXPECT_EQ(t_queue.back(), "World");
+    t_queue.push("!");
+    EXPECT_EQ(t_queue.back(), "!");
+    t_queue.pop();
+    EXPECT_EQ(t_queue.back(), "!");
+    t_queue.pop();
+    EXPECT_EQ(t_queue.back(), "!");
+    t_queue.pop();
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
